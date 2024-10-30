61f53670591c2edc0981650a822a37c6d6b5df66
Luis Lima <luisfernando.lima2005@gmail.com>
2024-10-30 20:29:27 -0300

    Criação do carrossel inicial

---
+++ public/js/home.mjs
@@ -0,0 +1,3 @@
+import { initializeCarousel } from "./models/slider.mjs";
+
+initializeCarousel()
---
+++ public/js/models/slider.mjs
@@ -0,0 +1,7 @@
+export function initializeCarousel(){
+    $(document).ready(function () {
+      $(".slide-salas").slick({
+        autoplay: true,
+      });
+    });
+}
