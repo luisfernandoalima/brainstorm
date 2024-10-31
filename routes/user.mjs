283e3bdd558bf36e4ae2709b0ae6d2a2493ffad3
Luis Lima <luisfernando.lima2005@gmail.com>
2024-10-31 19:11:36 -0300

    Início da página de perfil

--- routes/user.mjs
+++ routes/user.mjs
@@ -2,7 +2,7 @@ import express from "express"
 const router = express.Router()
 
 router.get('/perfil', (req, res)=>{
-
+    res.render('user/profile', {titulo: "Meu Perfil", css: "profile.css", js: "profile.mjs"})
 })
 
 export {router as userRouter}
