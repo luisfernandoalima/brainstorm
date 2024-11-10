348860fbded6c5c4bc9fe2073fa7a00cc9ea87ad
Luis Lima <luisfernando.lima2005@gmail.com>
2024-11-10 19:55:12 -0300

    Finalização da página de edição do perfil

--- public/js/editProfile.mjs
+++ public/js/editProfile.mjs
@@ -7,21 +7,105 @@ uploadInput.addEventListener("change", () => {
 });
 
 // Toastify
-import { initializeToastufy } from "./models/Toastify.mjs";
-initializeToastufy();
+import ToastifyPopUp from "./models/Toastify.mjs";
+var errorMessage = new ToastifyPopUp();
 
 // IMask
 import NewMask from "./models/Imask.mjs";
 
-const inputCell = new NewMask('#userCellInput', '(00) 00000-0000')
-inputCell.createMask()
+const inputCell = new NewMask("#userCellInput", "(00) 00000-0000");
+inputCell.createMask();
 
 // Change visibility
 import PasswordVisibility from "./models/PasswordVisibility.mjs";
-const passwordInput = new PasswordVisibility('#userPasswordInput', '.passwordViewButton')
+const passwordInput = new PasswordVisibility(
+  "#userPasswordInput",
+  ".passwordViewButton"
+);
 
-passwordInput.initializeChangeVisibility()
+passwordInput.initializeChangeVisibility();
 
-const repeatPasswordInput = new PasswordVisibility('#userRepeatPasswordInput', '.repeatPasswordViewButton')
+const repeatPasswordInput = new PasswordVisibility(
+  "#userRepeatPasswordInput",
+  ".repeatPasswordViewButton"
+);
 
-repeatPasswordInput.initializeChangeVisibility()
+repeatPasswordInput.initializeChangeVisibility();
+
+// Confirmar senha
+const inputPassword = document.querySelector("#userPasswordInput");
+const inputAreaPassword = document.querySelector(".repeat-area ");
+
+inputPassword.addEventListener("change", () => {
+  if (inputAreaPassword.classList.contains("disable")) {
+    inputAreaPassword.classList.remove("disable");
+  }
+});
+
+// Habilitar botão
+const btnSubmit = document.querySelector(".btn-submit");
+
+const checkErrors = () => {
+  let inputs = document.querySelectorAll("input");
+  let errorCount = 0;
+  let errorText = [];
+
+  inputs.forEach((input) => {
+    if (input.value == "" || input.value == null || input.value == undefined) {
+      errorCount += 1;
+    }
+  });
+  if (errorCount != 0) {
+    errorText.push("Os campos não podem estar vazios!");
+  }
+
+  let inputPassword = document.querySelector("#userPasswordInput");
+  let repeatInputPassword = document.querySelector("#userRepeatPasswordInput");
+
+  if (inputPassword.value != repeatInputPassword.value) {
+    errorCount += 1;
+    errorText.push("As senhas devem ser iguais!");
+  }
+
+  if (errorText.length > 0) {
+    return errorText;
+  } else {
+    return false;
+  }
+};
+
+btnSubmit.addEventListener("mouseover", () => {
+  if (btnSubmit.disabled === true) {
+    let error = checkErrors();
+
+    error.forEach((error) => {
+      errorMessage.showToastifyError(error);
+    });
+  }
+});
+
+const checkInputs = () => {
+  let inputs = document.querySelectorAll("input");
+  let errorCount = 0;
+
+  inputs.forEach((input) => {
+    if (input.value == "" || input.value == null || input.value == undefined) {
+      errorCount += 1;
+    }
+  });
+  
+  let inputPassword = document.querySelector("#userPasswordInput");
+  let repeatInputPassword = document.querySelector("#userRepeatPasswordInput");
+
+  if (inputPassword.value != repeatInputPassword.value) {
+    errorCount += 1;
+  }
+
+  if (errorCount == 0) {
+    btnSubmit.disabled = false;
+  } else {
+    btnSubmit.disabled = true;
+  }
+};
+
+setInterval(checkInputs, 500)
--- public/js/models/Toastify.mjs
+++ public/js/models/Toastify.mjs
@@ -1,16 +1,32 @@
-export function initializeToastufy() {
-  Toastify({
-    text: "This is a toast",
-    duration: 3000,
-    destination: "https://github.com/apvarun/toastify-js",
-    newWindow: true,
-    close: true,
-    gravity: "top", // `top` or `bottom`
-    position: "right", // `left`, `center` or `right`
-    stopOnFocus: true, // Prevents dismissing of toast on hover
-    style: {
-      background: "linear-gradient(to right, #00b09b, #96c93d)",
-    },
-    onClick: function () {}, // Callback after click
-  }).showToast();
+class ToastifyPopUp {
+  constructor(text) {
+    this.text = text;
+  }
+  showToastifySuccess(text) {
+    Toastify({
+      text: text,
+      duration: 3000,
+      gravity: "top", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "linear-gradient(to right, #00b09b, #96c93d)",
+      },
+    }).showToast();
+  }
+
+  showToastifyError(text) {
+    Toastify({
+      text: text,
+      duration: 3000,
+      gravity: "top", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "#d00000",
+      },
+    }).showToast();
+  }
 }
+
+export default ToastifyPopUp;
