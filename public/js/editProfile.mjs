797b5cded5b2458ed8bccade011b630a48fd74eb
Luis Lima <luisfernando.lima2005@gmail.com>
2024-11-10 14:46:40 -0300

    Criação da página de edição do perfil

--- public/js/editProfile.mjs
+++ public/js/editProfile.mjs
@@ -1,10 +1,27 @@
-const uploadInput = document.querySelector('#userImage');
-const imageTag = document.querySelector('#userPicture');
+const uploadInput = document.querySelector("#userImage");
+const imageTag = document.querySelector("#userPicture");
 
-uploadInput.addEventListener('change', () =>{
-    const file = event.target.files[0];
-    imageTag.src = URL.createObjectURL(file)
-})
+uploadInput.addEventListener("change", () => {
+  const file = event.target.files[0];
+  imageTag.src = URL.createObjectURL(file);
+});
 
+// Toastify
 import { initializeToastufy } from "./models/Toastify.mjs";
-initializeToastufy()
+initializeToastufy();
+
+// IMask
+import NewMask from "./models/Imask.mjs";
+
+const inputCell = new NewMask('#userCellInput', '(00) 00000-0000')
+inputCell.createMask()
+
+// Change visibility
+import PasswordVisibility from "./models/PasswordVisibility.mjs";
+const passwordInput = new PasswordVisibility('#userPasswordInput', '.passwordViewButton')
+
+passwordInput.initializeChangeVisibility()
+
+const repeatPasswordInput = new PasswordVisibility('#userRepeatPasswordInput', '.repeatPasswordViewButton')
+
+repeatPasswordInput.initializeChangeVisibility()
