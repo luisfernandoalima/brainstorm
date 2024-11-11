772ab37585f3d39757e6a9d8775e496cffed5358
Luis Lima <luisfernando.lima2005@gmail.com>
2024-11-11 16:56:20 -0300

    Criação da tela de login

--- routes/router.mjs
+++ routes/router.mjs
@@ -2,7 +2,15 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index", { titulo: "Página Inicial",css: "home.css", js: "home.mjs" });
+  res.render("index", {
+    titulo: "Página Inicial",
+    css: "home.css",
+    js: "home.mjs",
+  });
+});
+
+router.get("/login", (req, res) => {
+  res.render("login", { titulo: "Login", css: "login.css", js: "login.mjs" });
 });
 
 export default router;
